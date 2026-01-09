9b22403c7c9b1c663b2091886739fe3e78e20374
Jacob Andreas Reistad <97035496+jacob-andreas-reistad@users.noreply.github.com>
2026-01-09 06:52:19 +0100

    Update foo.c
    completed the main function.. I think.

--- 3 - shared variable/c/foo.c
+++ 3 - shared variable/c/foo.c
@@ -24,10 +24,14 @@ int main(){
     // TODO: 
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
-    
+    pthread_t th1, th2;
+    pthread_create(&th1, NULL, incrementingThreadFunction(), NULL);
+    pthread_create(&th2, NULL, decrementingThreadFunction(), NULL);
     // TODO:
     // wait for the two threads to be done before printing the final result
-    // Hint: Use `pthread_join`    
+    // Hint: Use `pthread_join`  
+    pthread_join(th1, NULL);
+    pthread_join(th2, NULL)
     
     printf("The magic number is: %d\n", i);
     return 0;
