d992dee93a4b5c1af412be96051baefb4183767a
Jacob Andreas Reistad <97035496+jacob-andreas-reistad@users.noreply.github.com>
2026-01-15 21:41:45 +0100

    Update foo.go

--- 3 - shared variable/go/foo.go
+++ 3 - shared variable/go/foo.go
@@ -3,36 +3,68 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
 )
 
-var i = 0
+type request int 
 
-func incrementing() {
-    //TODO: increment i 1000000 times
-    for j := 0; j < 1000000; j++ {
-            i++;
-    }
+const (
+	inc request = iota // starts inc at 0, then dec at 1 and get at 2
+	dec
+	get
+)
+
+func number_server(request_ch chan request, reply_ch chan int) {
+	i := 0
+
+	for {
+		select {
+		case r := <-request_ch:
+			switch r {
+			case inc:
+				i++
+			case dec:
+				i--
+			case get:
+				reply_ch <- i
+				return
+			}
+		}
+	}
 }
 
-func decrementing() {
-    //TODO: decrement i 1000000 times
-    for j := 0; j < 1000000; j++ {
-            i--;
-    }
+func incrementing(reques_ch chan request, done chan bool) {
+	for j := 0; j < 1000000; j++ {
+		reques_ch <- inc
+	}
+    done <- true
+}
+
+func decrementing(reques_ch chan request, done chan bool) {
+	for j := 0; j < 999958; j++ {
+		reques_ch <- dec
+	}
+    done <- true
 }
 
 func main() {
-    // What does GOMAXPROCS do? What happens if you set it to 1?
-    runtime.GOMAXPROCS(1)    
-	
-    // TODO: Spawn both functions as goroutines
-	go incrementing()
-	go decrementing()
-    // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
-    // We will do it properly with channels soon. For now: Sleep.
-    time.Sleep(500*time.Millisecond)
-    Println("The magic number is:", i)
+	// What does GOMAXPROCS do? What happens if you set it to 1?
+	runtime.GOMAXPROCS(1)
+
+	req_ch := make(chan request)
+	rep_ch := make(chan int)
+    done   := make(chan bool)
+
+	go number_server(req_ch, rep_ch)
+	go incrementing(req_ch, done)
+	go decrementing(req_ch, done)
+
+    <- done
+	<- done
+   
+	req_ch <- get
+	result := <-rep_ch
+
+	Println("The magic number is:", result)
 }
